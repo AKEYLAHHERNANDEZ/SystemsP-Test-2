749f4ea8b1319882f3035d2dce0aa4353eecab5b
AKEYLAHHERNANDEZ <119026429+AKEYLAHHERNANDEZ@users.noreply.github.com>
2025-04-09 20:36:30 +0000

    Add go routine and comments

--- main.go
+++ main.go
@@ -1,44 +1,51 @@
-//creating a tcp echo server 
+// creating a tcp echo server
 package main
 
-import(
+import (
+	"bufio"
+	"flag"
 	"fmt"
 	"net"
+	"os"
+	"strings"
+	"time"
 )
 
-func main(){
-	listener, err := net.Listen("tcp",":4000")
+func main() {
+	listener, err := net.Listen("tcp", ":4000")//change to an address
 	if err != nil {
 		panic(err)
 	}
 	defer listener.Close()
 
-fmt.Println("Server listening on :4000")
-for {
-	conn,err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting:", err)
-		continue
+	fmt.Println("Server listening on :4000")//change to an address
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting:", err)
+			continue
+		}
+		go handleConnection(conn)//addded go routine
 	}
-	handleConnection(conn)
-}
-}//infinite loop
+} //infinite loop
+
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024)
+	//buf := make([]byte, 1024)
+	//add the log connection for the timestamp
 
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading from client:", err)
-			return
-		}
-_, err = conn.Write(buf[:n])
-if err !=nil {
-	fmt.Println("Error writing to client:",err)
-}
-	}
+	// for {
+	// 	n, err := conn.Read(buf)
+	// 	if err != nil {
+	// 		fmt.Println("Error reading from client:", err)
+	// 		return
+	// 	}
+	// 	_, err = conn.Write(buf[:n])
+	// 	if err != nil {
+	// 		fmt.Println("Error writing to client:", err)
+	// 	}
+	// } remove these and implement mathods-2-9
 }
 
 //run the function using:
